dc91416cd2a662e464cc2c8da9d6d155913dadbb
telephone <telephone.tan@qq.com>
2023-06-04 17:16:05 +0800

    CacheMap 创建时指定容量阈值与缓存最大存活时间 | CacheMap Load 操作时刷新缓存活跃时间 | CacheMap Store 操作后如容量超过阈值则自动触发清理操作清理不活跃的缓存

--- maps/cacheMap.go
+++ maps/cacheMap.go
@@ -5,29 +5,40 @@ import (
 	"time"
 )
 
-type timeData[D any] struct {
+type _timeData[D any] struct {
 	time int64
 	data D
 }
+type timeData[D any] struct {
+	*_timeData[D]
+}
+
+func now() int64 {
+	return time.Now().UnixNano()
+}
 
 func newTimeData[D any](data D) timeData[D] {
-	return timeData[D]{time: time.Now().UnixNano(), data: data}
+	return timeData[D]{_timeData: &_timeData[D]{time: now(), data: data}}
 }
 
 type _CacheMap[K comparable, V any] struct {
-	Concurrent bool
-	m          map[K]timeData[V]
-	rLock      sync.Locker
-	wLock      sync.Locker
+	Concurrent    bool
+	SizeThreshold int
+	TTLNano       int64
+	m             map[K]timeData[V]
+	rLock         sync.Locker
+	wLock         sync.Locker
 }
 type CacheMap[K comparable, V any] struct {
 	*_CacheMap[K, V]
 }
 
-func NewCacheMap[K comparable, V any](concurrent bool, init ...func(CacheMap[K, V])) CacheMap[K, V] {
+func NewCacheMap[K comparable, V any](concurrent bool, sizeThreshold int, ttlNano int64, init ...func(CacheMap[K, V])) CacheMap[K, V] {
 	m := CacheMap[K, V]{
 		_CacheMap: &_CacheMap[K, V]{
-			Concurrent: concurrent,
+			Concurrent:    concurrent,
+			SizeThreshold: sizeThreshold,
+			TTLNano:       ttlNano,
 		},
 	}
 	if len(init) > 0 {
@@ -49,8 +60,18 @@ func (s *CacheMap[K, V]) Load(key K) (value V, ok bool) {
 	x, ok := s.m[key]
 	if ok {
 		value = x.data
+		x.time = now()
 	}
 	return value, ok
+}
+
+func (s *CacheMap[K, V]) clear() {
+	n := now()
+	for k, v := range s.m {
+		if n-v.time > s.TTLNano {
+			delete(s.m, k)
+		}
+	}
 }
 
 func (s *CacheMap[K, V]) Store(key K, value V) {
@@ -61,6 +82,9 @@ func (s *CacheMap[K, V]) Store(key K, value V) {
 		defer s.wLock.Unlock()
 	}
 	s.m[key] = newTimeData(value)
+	if len(s.m) > s.SizeThreshold {
+		s.clear()
+	}
 }
 
 func (s *CacheMap[K, V]) LoadOrStore(key K, value V) (actual V, loaded bool) {
@@ -76,6 +100,8 @@ func (s *CacheMap[K, V]) LoadOrStore(key K, value V) (actual V, loaded bool) {
 		}
 		x = newTimeData(value)
 		s.m[key] = x
+	} else {
+		x.time = now()
 	}
 	actual = x.data
 	return actual, loaded
