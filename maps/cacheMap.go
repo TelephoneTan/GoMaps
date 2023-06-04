60b50b3f778d49d1a6801c22febd8cee80bbfd86
telephone <telephone.tan@qq.com>
2023-06-04 18:31:27 +0800

    CacheMap 中的 TTL 改用 time.Duration 类型表示

--- maps/cacheMap.go
+++ maps/cacheMap.go
@@ -6,25 +6,25 @@ import (
 )
 
 type _timeData[D any] struct {
-	time int64
-	data D
+	tsNano int64
+	data   D
 }
 type timeData[D any] struct {
 	*_timeData[D]
 }
 
-func now() int64 {
+func nowNano() int64 {
 	return time.Now().UnixNano()
 }
 
 func newTimeData[D any](data D) timeData[D] {
-	return timeData[D]{_timeData: &_timeData[D]{time: now(), data: data}}
+	return timeData[D]{_timeData: &_timeData[D]{tsNano: nowNano(), data: data}}
 }
 
 type _CacheMap[K comparable, V any] struct {
 	Concurrent    bool
 	SizeThreshold int
-	TTLNano       int64
+	TTL           time.Duration
 	m             map[K]timeData[V]
 	rLock         sync.Locker
 	wLock         sync.Locker
@@ -33,12 +33,12 @@ type CacheMap[K comparable, V any] struct {
 	*_CacheMap[K, V]
 }
 
-func NewCacheMap[K comparable, V any](concurrent bool, sizeThreshold int, ttlNano int64, init ...func(CacheMap[K, V])) CacheMap[K, V] {
+func NewCacheMap[K comparable, V any](concurrent bool, sizeThreshold int, ttl time.Duration, init ...func(CacheMap[K, V])) CacheMap[K, V] {
 	m := CacheMap[K, V]{
 		_CacheMap: &_CacheMap[K, V]{
 			Concurrent:    concurrent,
 			SizeThreshold: sizeThreshold,
-			TTLNano:       ttlNano,
+			TTL:           ttl,
 		},
 	}
 	if len(init) > 0 {
@@ -60,15 +60,15 @@ func (s *CacheMap[K, V]) Load(key K) (value V, ok bool) {
 	x, ok := s.m[key]
 	if ok {
 		value = x.data
-		x.time = now()
+		x.tsNano = nowNano()
 	}
 	return value, ok
 }
 
 func (s *CacheMap[K, V]) clear() {
-	n := now()
+	n := nowNano()
 	for k, v := range s.m {
-		if n-v.time > s.TTLNano {
+		if n-v.tsNano > s.TTL.Nanoseconds() {
 			delete(s.m, k)
 		}
 	}
@@ -101,7 +101,7 @@ func (s *CacheMap[K, V]) LoadOrStore(key K, value V) (actual V, loaded bool) {
 		x = newTimeData(value)
 		s.m[key] = x
 	} else {
-		x.time = now()
+		x.tsNano = nowNano()
 	}
 	actual = x.data
 	return actual, loaded
